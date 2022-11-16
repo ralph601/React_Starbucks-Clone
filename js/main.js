dc6b32d7237fcdf6d32bbd41e2fe15629c480494
ralph601 <ralph601@naver.com>
2022-11-16 16:04:10 +0900

    DropDown Menu-검색 수정

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 const searchEl = document.querySelector('.search');
-const searchInput=searchEl.querySelector('input');
+const searchInputEl=searchEl.querySelector('input');
+
+searchEl.addEventListener('click',function (){
+  //Logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus',function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+});
+
+searchInputEl.addEventListener('blur',function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
+});
