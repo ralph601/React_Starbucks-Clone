2760c4f1cbc5084a15de15e5a3b2cfe60e8464e8
ralph601 <jeff0101@naver.com>
2022-11-24 11:30:01 +0900

    scrollToPlugin update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur',function() {
 });
 
 const badgeEl=document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){ /*함수 실행 시 과부하 방지*/
   console.log(window.scrollY);
@@ -26,16 +27,30 @@ window.addEventListener('scroll', _.throttle(function(){ /*함수 실행 시 과
       opacity: 0,
       display: 'none'
     });
+    //버튼 보이기
+    gsap.to(toTopEl,.2,{
+      x:0
+    });
   } else{
     //배지 보이기
     gsap.to(badgeEl, .6, {
       opacity:1,
       display:'block'
     });
+    //버튼 숨기기
+    gsap.to(toTopEl,.2,{
+      x:100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
 
+
+toTopEl.addEventListener('click',function(){
+  gsap.to(window, .7, {
+    scrollTo:0,
+  });
+});
 
 // 나타날 요소들(.fade-in) 찾기.
 const fadeEls = document.querySelectorAll('.visual .fade-in')
@@ -130,4 +145,8 @@ spyEls.forEach(function(spyEl){
   })
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller())
-})
+});
+
+
+const thisyear=document.querySelector('.this-year');
+thisyear.textContent = new Date().getFullYear(); 
